a95ac089e4b83b84927ee94b100801859889b474
Cao Xuan Phong <caoxuanphong@Caos-MacBook-Pro.local>
2017-04-20 15:21:27 +0700

    update

--- AndroidUtils.java
+++ AndroidUtils.java
@@ -1,21 +1,34 @@
-dpToPixel()
-pxelToDp()
-screenWidth()
-screenHeight()
-makeScreenAlwaysOn();
-getBattery()
-getTotalMemory()
-getFreeMemory()
-isPhoneOrTablet()
-getModel()
-getAndroidversion()
-getIPAddress()
-getBatteryStatus()
-getLanguage()
-getUserName()
-getPhoneNumber()
-getCurrentWifi()
-getSIMInfo();
-getNumberPhotos()
-getNumberImage()
-getNumberVideo()
+//dpToPixel()
+//pxelToDp()
+//screenWidth()
+//screenHeight()
+//makeScreenAlwaysOn();
+//getBattery()
+//getTotalMemory()
+//getFreeMemory()
+//isPhoneOrTablet()
+//getModel()
+//getAndroidversion()
+//getIPAddress()
+//getBatteryStatus()
+//getLanguage()
+//getUserName()
+//getPhoneNumber()
+//getCurrentWifi()
+//getSIMInfo();
+//getNumberPhotos()
+//getNumberImage()
+//getNumberVideo()
+
+package common.android.fiot.androidcommon;
+
+public class AndroidUtils {
+
+	/**
+	 * Keep screen always on
+	 * Call in @onCreate() of current Activity, call befor @setContentView()	
+	 */
+	public static void keepScreenAlwaysOn() {
+		getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+	}
+}
